4e29af18aaf902fd715b0918da2a357acf400bf7
leslieFhub <134671364+leslieFhub@users.noreply.github.com>
2023-05-30 11:41:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,9 +13,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-app.use(express.static(__dirname));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
