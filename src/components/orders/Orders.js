0956acdd57e84ebb94faf8b8c823e9241ab18451
leslieFhub <134671364+leslieFhub@users.noreply.github.com>
2023-06-12 17:23:15 +0800

    Update Orders.js

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { useDispatch } from "react-redux";
 import { deleteOrder } from "../../Redux/Actions/OrderActions";
+
+const OrderStatistics = ({ orders, orderType }) => {
+  const filteredOrders = orderType === "all"
+    ? orders
+    : orders.filter((order) => {
+        if (orderType === "dine-in" && order.user.email.includes("table")) {
+          return true;
+        }
+        if (orderType === "online" && !order.user.email.includes("table")) {
+          return true;
+        }
+        return false;
+      });
+
+  const totalOrders = filteredOrders.length;
+
+  return (
+    <div className="order-statistics">
+      <h4>{orderType.charAt(0).toUpperCase() + orderType.slice(1)} Orders</h4>
+      <p>Total Orders: {totalOrders}</p>
+    </div>
+  );
+};
 
 const Orders = (props) => {
   const { orders } = props;
   const [orderType, setOrderType] = useState("all"); // all, dine-in, online
   const [searchQuery, setSearchQuery] = useState("");
   const [sortOrder, setSortOrder] = useState("desc"); // desc (latest to oldest), asc (oldest to latest)
+  const [selectedDate, setSelectedDate] = useState(null); // selected date state
   const dispatch = useDispatch();
 
   const filteredOrders = orderType === "all"
@@ -34,12 +60,6 @@ const Orders = (props) => {
     }
   });
 
-  const deleteHandler = (id) => {
-    if (window.confirm("Are you sure?")) {
-      dispatch(deleteOrder(id));
-    }
-  };
-
   const printOrders = () => {
     const printContent = document.getElementById("orders-table").innerHTML;
     const originalContent = document.body.innerHTML;
@@ -54,9 +74,27 @@ const Orders = (props) => {
     setSortOrder(newSortOrder);
   };
 
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const filterOrdersByDate = (orders) => {
+    if (!selectedDate) {
+      return orders;
+    }
+    const selectedDayStart = moment(selectedDate).startOf("day");
+    const selectedDayEnd = moment(selectedDate).endOf("day");
+    return orders.filter((order) => {
+      const orderDate = moment(order.createdAt);
+      return orderDate.isBetween(selectedDayStart, selectedDayEnd, undefined, "[]");
+    });
+  };
+
+  const filteredOrdersByDate = filterOrdersByDate(sortedOrders);
+
   return (
     <div>
-      <div className="d-flex justify-content-end mb-3">
+      <div className="d-flex justify-content-between align-items-center mb-3">
         <input
           type="text"
           placeholder="Search orders by name..."
@@ -64,6 +102,36 @@ const Orders = (props) => {
           onChange={(e) => setSearchQuery(e.target.value)}
           className="form-control p-2"
         />
+      </div>
+      <label className="me-2">Filter by Date:</label>
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          dateFormat="MMM d, yyyy"
+          className="form-control date-picker"
+          placeholderText="Select a date"
+        />
+      </div>
+
+
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="card card-body mb-4 shadow-sm">
+            <OrderStatistics orders={orders} orderType="all" />
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="card card-body mb-4 shadow-sm">
+            <OrderStatistics orders={orders} orderType="dine-in" />
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="card card-body mb-4 shadow-sm">
+            <OrderStatistics orders={orders} orderType="online" />
+          </div>
+        </div>
       </div>
 
       <div className="btn-group mb-3" role="group">
@@ -133,15 +201,16 @@ const Orders = (props) => {
             </tr>
           </thead>
           <tbody>
-            {sortedOrders.map((order) => (
+            {filteredOrdersByDate.map((order) => (
               <tr key={order._id}>
                 <td>
                   <b>{order.user.name}</b>
                 </td>
-                <td><b>{order._id}</b></td>
-                
+                <td>
+                  <b>{order._id}</b>
+                </td>
                 <td>{order.user.email}</td>
-                <td>Php {order.totalPrice}</td>
+                <td>Php {Number(order.totalPrice).toFixed(2)}</td>
                 <td>{order.paymentMethod}</td>
                 <td>
                   {order.isPaid ? (
@@ -149,9 +218,7 @@ const Orders = (props) => {
                       Paid At {moment(order.paidAt).format("MMM Do YY")}
                     </span>
                   ) : (
-                    <span className="badge rounded-pill alert-danger">
-                      Not Paid
-                    </span>
+                    <span className="badge rounded-pill alert-danger">Not Paid</span>
                   )}
                 </td>
                 <td>{moment(order.createdAt).format("MMM Do YY")}</td>
@@ -172,8 +239,6 @@ const Orders = (props) => {
           </tbody>
         </table>
       </div>
-      
-      
     </div>
   );
 };
